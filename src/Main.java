2292317b9791d091e332b51f52efe84124cedb9d
Pablo Vergara <vergarapablo2001@gmail.com>
2022-05-21 14:36:02 -0500

    first push

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import iteso.libs.panel.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+//		int idUser = 1;
+
+		PanelLogIn.createPanelLogIn();
+//		PanelMenu.createPanelMenu(idUser);
+//		PanelSettings.createPanelSettings(idUser);
+//		PanelInputsInventory.createPanelInputsInventory(idUser);
+//		PanelOutputsInventory.createPanelOutputsInventory(idUser);
+//		PanelProducts.createPanelProducts(idUser);
+//		PanelAddNewProduct.createPanelAddNewProduct(idUser);
+//		PanelEditProduct.createPanelEditProduct(idUser);
+//		PanelDeleteProduct.createPanelDeleteProduct(idUser);
+//		PanelInventory.createPanelInventory(idUser);
+//		PanelRecord.createPanelRecord(idUser);
+//		PanelAccountsManager.createPanelAccountsManager(idUser);
+//		PanelAddNewUser.createPanelAddNewUser(idUser);
+//		PanelDeleteUser.createPanelDeleteUser(idUser);
+//		PanelShowUsers.createPanelShowUsers(idUser);
+
+	}
+
+}
